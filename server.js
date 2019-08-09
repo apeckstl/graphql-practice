ba86057fc2440c46fbdaf93832157066ae17a7da
Avery Peck <averypeck@northwesternmutual.com>
2019-08-09 08:37:44 -0500

    add react app

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const router = require('./routes/router');
+
+app.use('/', router);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');

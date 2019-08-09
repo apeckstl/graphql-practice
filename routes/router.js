a888a75704e071b515de2cd7d158b6f14d97a8e7
Avery Peck <averypeck@northwesternmutual.com>
2019-08-09 15:23:16 -0500

    Add graphql

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const cors = require('cors');
+const { db } = require('../pgAdaptor.js');
 
 router.get('/', cors(), function(req, res, next) {
-    res.send('API is working properly');
+    db.many('select * from book').then(list => {
+        res.status(200);
+        res.send(list);
+    });
+    res.status(500);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
+const graphql = require("graphql");
 const router = require('./routes/router');
 const cookieParser = require('cookie-parser');
+const expressGraphQl = require("express-graphql");
+const { GraphQLSchema } = graphql;
+const { query } = require("./schemas/queries");
+
+const schema = new GraphQLSchema({
+  query
+});
+
+app.use(
+  '/graphql',
+  expressGraphQl({
+    schema: schema,
+    graphiql: true
+  })
+);
 
 app.use('/', router);
 app.use(express.json());

9e936d0e5b95584a4948f512ac5483459aaf980e
avery <averypeckstl@gmail.com>
2019-08-09 22:40:23 -0500

    fix type to match database

--- schemas/types.js
+++ schemas/types.js
@@ -1,14 +1,19 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLInt, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLFloat } = graphql;
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   type: "Query",
   fields: {
     id: { type: GraphQLInt },
-    name: { type: GraphQLString },
-    author: { type: GraphQLString },
-    year_published: { type: GraphQLInt }
+    title: { type: GraphQLString },
+    isbn13: { type: GraphQLString },
+    publisher: { type: GraphQLString },
+    image_url: { type: GraphQLString },
+    num_pages: { type: GraphQLInt },
+    average_rating: { type: GraphQLFloat },
+    description: { type: GraphQLString },
+    published: { type: GraphQLInt }
   }
 });
 
